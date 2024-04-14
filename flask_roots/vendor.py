57c9523afb4faa32bb91e9fdbf5f63a250b2ca29
Mike Boers <mikeb@immersionroom.com>
2024-04-14 19:24:05 -0400

    [wip] Fixes for Flask 3

--- flask_roots/vendor.py
+++ flask_roots/vendor.py
@@ -8,7 +8,7 @@ def init_flask_login(app):
     login = LoginManager(app)
     app.roots['login_mananger'] = login # Old name.
     app.roots['authn'] = login # New name.
-    login.user_callback = lambda uid: None # TODO: Why?
+    #login.user_callback = lambda uid: None # TODO: Why?
 
 
 def init_flask_acl(app):
@@ -37,7 +37,7 @@ def init_flask_images(app):
 def init_flask_sqlalchemy(app):
     from flask_sqlalchemy import SQLAlchemy
     app.roots['db'] = db = SQLAlchemy(app)
-    db.metadata.bind = db.engine # TODO: Why?
+    #db.metadata.bind = db.engine # TODO: Why?
 
 
 def init_flask_mail(app):
